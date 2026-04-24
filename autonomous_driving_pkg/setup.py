2716423dec4a77032cb0b65b78ade95b7b0824a6
Recep Yalçınkaya <137687914+recepyalcinkaya@users.noreply.github.com>
2026-04-24 09:13:59 +0300

    Add setup.py for autonomous driving package
    Added setup configuration for the autonomous driving package including metadata and entry points.

--- autonomous_driving_pkg/setup.py
+++ autonomous_driving_pkg/setup.py
@@ -1,1 +1,29 @@
+from setuptools import setup
+import os
+from glob import glob
 
+package_name = 'autonomous_driving_pkg'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='Recep Yalçınkaya',
+    maintainer_email='receptalha0899@gmail.com',
+    description='ROS 2 Autonomous Driving Integration Package',
+    license='Apache License 2.0',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'vehicle_brain = autonomous_driving_pkg.vehicle_brain:main',
+            'sensor_publisher = autonomous_driving_pkg.sensor_publisher:main'
+        ],
+    },
+)
